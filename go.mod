a7b045a4440170bf552e8024fff46163efd148c2
Frank Ittermann <frank.ittermann@yahoo.de>
2020-02-26 22:18:22 +0100

    Add ec2 instance creation

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module main
 go 1.13
 
 require (
-	github.com/pulumi/pulumi v1.10.1
-	github.com/pulumi/pulumi-aws v1.21.0
+	github.com/pulumi/pulumi v1.11.1
+	github.com/pulumi/pulumi-aws v1.23.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,71 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/go/aws"
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/go/pulumi"
 )
 
+const size = "t2.micro"
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return pulumiForAutomation(ctx)
+		err := pulumiForAutomation(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		return createJenkinsVM(ctx)
 	})
+}
+
+func createJenkinsVM(ctx *pulumi.Context) error {
+	group, err := ec2.NewSecurityGroup(ctx, "web-secgrp-2", &ec2.SecurityGroupArgs{
+		Description: pulumi.String("Enable HTTP access"),
+		Ingress: ec2.SecurityGroupIngressArray{
+			ec2.SecurityGroupIngressArgs{
+				Protocol:   pulumi.String("tcp"),
+				FromPort:   pulumi.Int(80),
+				ToPort:     pulumi.Int(80),
+				CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	mostRecent := true
+	ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
+		Filters: []aws.GetAmiFilter{
+			{
+				Name:   "name",
+				Values: []string{"	ami-078603b469de54ad7"},
+			},
+		},
+		MostRecent: &mostRecent,
+	})
+	if err != nil {
+		return err
+	}
+
+	server, err := ec2.NewInstance(ctx, "web-server-www", &ec2.InstanceArgs{
+		InstanceType: pulumi.String(size),
+		SecurityGroups: pulumi.StringArray{
+			group.Name,
+		},
+		Ami: pulumi.String(ami.Id),
+		UserData: pulumi.String(`#cloud-config
+`),
+	})
+
+	ctx.Export("publicIp", server.PublicIp)
+	ctx.Export("publicDns", server.PublicDns)
+
+	return err
 }
 
 func pulumiForAutomation(ctx *pulumi.Context) error {
