4a00bd4b202b46c5178305dac2465f30ccccd796
Frank Ittermann <frank.ittermann@yahoo.de>
2021-08-15 18:00:11 +0200

    Fix invalid aws credentials

--- jenkins-ec2/jenkins-ec2.go
+++ jenkins-ec2/jenkins-ec2.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+    "fmt"
+    "os"
+    "time"
 
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/actors"
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/aws/compute"
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/aws/network"
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/model"
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/utility"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/actors"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/aws/compute"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/aws/network"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/model"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/utility"
+    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+    "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 const size = "t2.large"
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		// config := config.New(ctx, "")
+    pulumi.Run(func(ctx *pulumi.Context) error {
+        // config := config.New(ctx, "")
 
-		// awsKeyID := config.Require("key")
-		// awsKeySecret := config.Require("secret")
-		return createInfraStructure(ctx)
-	})
+        // awsKeyID := config.Require("key")
+        // awsKeySecret := config.Require("secret")
+        return createInfraStructure(ctx)
+    })
 }
 
 func exports(ctx *pulumi.Context, infra *compute.Infrastructure) {
@@ -32,106 +32,106 @@ func exports(ctx *pulumi.Context, infra *compute.Infrastructure) {
 }
 
 func createInfraStructure(ctx *pulumi.Context) error {
-	config := config.New(ctx, "")
+    config := config.New(ctx, "")
 
-	//TODO fetch new created aws key and secret
-	// awsKeyID := config.Require("key")
-	// awsKeySecret := config.Require("secret")
-	userDataEnvVariables := map[string]string{
-		"{{ ADMIN_PASSWORD }}":   "ADMIN_PASSWORD",
-		"{{ SEED_BRANCH_JOBS }}": "SEED_BRANCH_JOBS",
-	}
+    //TODO fetch new created aws key and secret
+    // awsKeyID := config.Require("key")
+    // awsKeySecret := config.Require("secret")
+    userDataEnvVariables := map[string]string{
+        "{{ ADMIN_PASSWORD }}":   "ADMIN_PASSWORD",
+        "{{ SEED_BRANCH_JOBS }}": "SEED_BRANCH_JOBS",
+    }
 
-	userDataSetVariables := map[string]string{
-		"{{ AWS_KEY_ID }}":     fmt.Sprintf("AWS_KEY_ID=%s", "undefined"),
-		"{{ AWS_KEY_SECRET }}": fmt.Sprintf("AWS_KEY_SECRET=%s", "undefined"),
-	}
+    userDataSetVariables := map[string]string{
+        "{{ AWS_KEY_ID }}":     fmt.Sprintf("AWS_KEY_ID=%s", "undefined"),
+        "{{ AWS_KEY_SECRET }}": fmt.Sprintf("AWS_KEY_SECRET=%s", "undefined"),
+    }
 
-	userData, err := model.NewUserData("cloud-init/cloud-init.yaml", append(model.TemplateVariablesEnvironment(userDataEnvVariables), model.TemplateVariablesString(userDataSetVariables)...))
-	if err != nil {
-		return err
-	}
+    userData, err := model.NewUserData("cloud-init/cloud-init.yaml", append(model.TemplateVariablesEnvironment(userDataEnvVariables), model.TemplateVariablesString(userDataSetVariables)...))
+    if err != nil {
+        return err
+    }
 
-	security := model.NewSecurityArgsForVPC(config.GetBool("vpn_enabled_ssh"), model.VPCArgsDefault)
-	security.Println()
+    security := model.NewSecurityArgsForVPC(config.GetBool("vpn_enabled_ssh"), model.VPCArgsDefault)
+    security.Println()
 
-	vpc, err := network.CreateVPC(ctx, model.VPCArgsDefault)
-	if err != nil {
-		return err
-	}
+    vpc, err := network.CreateVPC(ctx, model.VPCArgsDefault)
+    if err != nil {
+        return err
+    }
 
-	keyPairName := "development"
-	keyPair := model.NewKeyPairArgsWithRandomNameAndKey(&keyPairName)
-	computeArgs := model.NewComputeArgsWithKeyPair(vpc, security, keyPair)
-	computeArgs.Name = "jenkins-master"
-	computeArgs.UserData = userData
-	computeArgs.Images = []*model.ImageArgs{
-		{
-			Name:   "ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*",
-			Owners: []string{"099720109477"},
-		},
-	}
+    keyPairName := "development"
+    keyPair := model.NewKeyPairArgsWithRandomNameAndKey(&keyPairName)
+    computeArgs := model.NewComputeArgsWithKeyPair(vpc, security, keyPair)
+    computeArgs.Name = "jenkins-master"
+    computeArgs.UserData = userData
+    computeArgs.Images = []*model.ImageArgs{
+        {
+            Name:   "ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*",
+            Owners: []string{"099720109477"},
+        },
+    }
 
-	tags := map[string]string{
-		"JobUrl":         os.Getenv("TRAVIS_JOB_WEB_URL"),
-		"Project":        "jenkins",
-		"pulumi-managed": "True",
-	}
+    tags := map[string]string{
+        "JobUrl":         os.Getenv("TRAVIS_JOB_WEB_URL"),
+        "Project":        "jenkins",
+        "pulumi-managed": "True",
+    }
 
-	externalSecurityGroup := model.SecurityGroup{
-		Name:        "jenkins-security-group",
-		Description: "Pulumi Managed.",
-		Tags:        tags,
-		IngressRules: []*model.SecurityRule{
-			model.AllowOnePortRule("tcp", 80),
-			model.AllowOnePortRule("tcp", 22).CidrBlock("95.90.244.46/32"),
-			model.AllowSSHRule(security),
-		},
-		EgressRules: []*model.SecurityRule{
-			model.AllowOnePortRule("tcp", 80),
-			model.AllowOnePortRule("tcp", 443),
-			model.AllowOnePortRule("tcp", 22),
-			model.AllowOnePortRule("tcp", 22).CidrBlock("140.82.118.0/24"),
-			model.AllowOnePortRule("tcp", 22).CidrBlock("140.82.121.4/32"),
-			model.AllowOnePortRule("tcp", 22).CidrBlock("204.232.175.90/32"),
-			model.AllowOnePortRule("tcp", 22).CidrBlock("207.97.227.239/32"),
-		},
-	}
-	//The order is important the referenced security groups has to be first.
-	computeArgs.SecurityGroups = []*model.SecurityGroup{
-		&externalSecurityGroup,
-	}
+    externalSecurityGroup := model.SecurityGroup{
+        Name:        "jenkins-security-group",
+        Description: "Pulumi Managed.",
+        Tags:        tags,
+        IngressRules: []*model.SecurityRule{
+            model.AllowOnePortRule("tcp", 80),
+            model.AllowOnePortRule("tcp", 22).CidrBlock("95.90.244.46/32"),
+            model.AllowSSHRule(security),
+        },
+        EgressRules: []*model.SecurityRule{
+            model.AllowOnePortRule("tcp", 80),
+            model.AllowOnePortRule("tcp", 443),
+            model.AllowOnePortRule("tcp", 22),
+            model.AllowOnePortRule("tcp", 22).CidrBlock("140.82.118.0/24"),
+            model.AllowOnePortRule("tcp", 22).CidrBlock("140.82.121.4/32"),
+            model.AllowOnePortRule("tcp", 22).CidrBlock("204.232.175.90/32"),
+            model.AllowOnePortRule("tcp", 22).CidrBlock("207.97.227.239/32"),
+        },
+    }
+    //The order is important the referenced security groups has to be first.
+    computeArgs.SecurityGroups = []*model.SecurityGroup{
+        &externalSecurityGroup,
+    }
 
-	vm, err := compute.CreateServer(ctx, computeArgs, exports)
+    vm, err := compute.CreateServer(ctx, computeArgs, exports)
 
-	if err != nil {
-		return err
-	}
+    if err != nil {
+        return err
+    }
 
-	sshConnector := actors.NewSSHConnector(
-		actors.SSHConnectorArgs{
-			Port:       22,
-			Username:   "ubuntu",
-			Timeout:    2 * time.Minute,
-			SSHKeyPair: *keyPair.SSHKeyPair,
-			Commands: []actors.SSHCommand{
-				{
-					Command: "sudo cloud-init status --wait",
-					Output:  false,
-				},
-			},
-		},
-		utility.Logger{
-			Ctx: ctx,
-		},
-	)
+    sshConnector := actors.NewSSHConnector(
+        actors.SSHConnectorArgs{
+            Port:       22,
+            Username:   "ubuntu",
+            Timeout:    2 * time.Minute,
+            SSHKeyPair: *keyPair.SSHKeyPair,
+            Commands: []actors.SSHCommand{
+                {
+                    Command: "sudo cloud-init status --wait",
+                    Output:  false,
+                },
+            },
+        },
+        utility.Logger{
+            Ctx: ctx,
+        },
+    )
 
-	compute.ProvisionVM(ctx, &model.ProvisionArgs{
-		ExportName: "wireguard.publicKey",
-		SourceCompute: &model.ComputeResult{
-			Compute: vm.Server.CustomResourceState,
-		},
-	}, &sshConnector)
+    compute.ProvisionVM(ctx, &model.ProvisionArgs{
+        ExportName: "wireguard.publicKey",
+        SourceCompute: &model.ComputeResult{
+            Compute: vm.Server.CustomResourceState,
+        },
+    }, &sshConnector)
 
-	return err
+    return err
 }
